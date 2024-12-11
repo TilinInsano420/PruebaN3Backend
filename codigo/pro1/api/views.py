a94e3f0236da86d2e14dd469f5f624bbd9624895
Dylan Carmona <169170035+TilinInsano420@users.noreply.github.com>
2024-12-11 15:37:10 -0300

    Prueba final

--- codigo/pro1/api/views.py
+++ codigo/pro1/api/views.py
@@ -1,5 +1,5 @@
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from rest_framework import status
 from app.models import User, modelo_mas, Consulta
 from .serializers import UserSerializer, MascotaSerializer, ConsultaSerializer
